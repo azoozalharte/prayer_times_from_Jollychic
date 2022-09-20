f4e66c727a594803ec266892e3c19a404bdffda3
azoozalharte <zezo50115@protonmail.com>
2022-09-20 22:12:36 +0300

    get today prayers

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 "use strict";
 const req = new XMLHttpRequest();
 // get today date
-const date = moment().format();
-console.log(data);
-// req.addEventListener("readystatechange", (e) => {
-//   if (e.target.readyState === 4) {
-//     const data = JSON.parse(e.target.responseText);
-//     const todayPrayer = data.filter(prayer => prayer.)
-//   }
-// });
+const date = moment();
 
-// req.open(
-//   "get",
-//   `http://api.aladhan.com/v1/calendarByCity?city=ta'if%27if&country=sa&method=${date.getDate}&month=${date.getMonth}&year=${date.getFullYear}`
-// );
-// req.send();
+req.addEventListener("readystatechange", (e) => {
+  if (e.target.readyState === 4) {
+    const data = JSON.parse(e.target.responseText);
+    const getTodayPrayers = data.data.filter(
+      (prayer) => prayer.date.readable.split(" ")[0] === date.date().toString()
+    );
+    console.log(getTodayPrayers);
+  }
+});
+req.open(
+  "get",
+  `http://api.aladhan.com/v1/calendarByCity?city=ta'if%27if&country=sa&method=${date.date()}&month=${date.month()}&year=${date.year()}`
+);
+req.send();
 
 // http://api.aladhan.com/v1/calendarByCity?city=London&country=United%20Kingdom&method=2&month=04&year=2017
