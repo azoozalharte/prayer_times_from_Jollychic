3730c0227080d4f64d9a1ab5db40199b9aeb355a
azoozalharte <zezo50115@protonmail.com>
2022-09-21 00:12:06 +0300

    card header form js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 "use strict";
+moment.locale();
 const req = new XMLHttpRequest();
 // get today date
 const date = moment();
-
 req.addEventListener("readystatechange", (e) => {
   if (e.target.readyState === 4 && e.target.status === 200) {
     // console.log(e.target);
@@ -10,6 +10,30 @@ req.addEventListener("readystatechange", (e) => {
     const getTodayPrayers = data.data.filter(
       (prayer) => prayer.date.readable.split(" ")[0] === date.date().toString()
     );
+    console.log(getTodayPrayers);
+    // card header and content
+    const cardHeader = document.createElement("div");
+    cardHeader.classList.add("card-header");
+
+    const cityName = document.createElement("p");
+    cityName.classList.add("city-name");
+    cityName.innerHTML = "أوقات الصلاة في مدينة جدة";
+    cardHeader.appendChild(cityName);
+
+    const cityTime = document.createElement("p");
+    cityTime.classList.add("time");
+    cityTime.innerHTML = date.format("LT").includes("AM")
+      ? date.format("LT").replace("AM", "")
+      : date.format("LT").replace("PM", "");
+    cardHeader.appendChild(cityTime);
+
+    const uiDate = document.createElement("p");
+    uiDate.classList.add("ui-date");
+    uiDate.innerHTML = `${date.year()}/${date.month() + 1}/${date.date()}`;
+    cardHeader.appendChild(uiDate);
+    // prayer card
+
+    document.querySelector(".card").appendChild(cardHeader);
   }
 });
 req.open(
