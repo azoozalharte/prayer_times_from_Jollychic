371ffc77aa5c9cf3d9dd2233edfafb0f3061f19f
azoozalharte <zezo50115@protonmail.com>
2022-09-20 21:09:32 +0300

    moment js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 "use strict";
 const req = new XMLHttpRequest();
 // get today date
-const date = new Date();
+const date = moment().format();
+console.log(data);
+// req.addEventListener("readystatechange", (e) => {
+//   if (e.target.readyState === 4) {
+//     const data = JSON.parse(e.target.responseText);
+//     const todayPrayer = data.filter(prayer => prayer.)
+//   }
+// });
 
-req.addEventListener("readystatechange", (e) => {
-  if (e.target.readyState === 4) {
-    const data = JSON.parse(e.target.responseText);
-  }
-});
-
-req.open(
-  "get",
-  `http://api.aladhan.com/v1/calendarByCity?city=ta'if%27if&country=sa&method=${date.getDate}&month=${date.getMonth}&year=${date.getFullYear}`
-);
-req.send();
+// req.open(
+//   "get",
+//   `http://api.aladhan.com/v1/calendarByCity?city=ta'if%27if&country=sa&method=${date.getDate}&month=${date.getMonth}&year=${date.getFullYear}`
+// );
+// req.send();
 
 // http://api.aladhan.com/v1/calendarByCity?city=London&country=United%20Kingdom&method=2&month=04&year=2017
