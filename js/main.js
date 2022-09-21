0fe5aa2721e67df33e2d6d6abde5016168f55ed8
azoozalharte <zezo50115@protonmail.com>
2022-09-21 13:37:56 +0300

    DONE

--- js/main.js
+++ js/main.js
@@ -15,9 +15,17 @@ async function fetchData() {
     const getTodayPrayers = data.data.filter(
       (prayer) => prayer.date.readable.split(" ")[0] === date.date().toString()
     );
-    domGenerator(getTodayPrayers, city);
+
+    return getTodayPrayers;
   }
 }
+document.querySelector("#search").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  console.log(e.target.name.value);
+  city = e.target.name.value;
+  const result = await fetchData();
+  domGenerator(result, city);
+});
 
 fetchData();
 // http://api.aladhan.com/v1/calendarByCity?city=London&country=United%20Kingdom&method=2&month=04&year=2017
